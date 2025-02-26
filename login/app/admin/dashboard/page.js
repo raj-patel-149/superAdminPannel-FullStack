10bed8e81c21ec1dd9860ecd89324fc2fad16bea
raj-patel-149 <rdpatel11124@gmail.com>
2025-02-26 11:28:03 +0530

    delete old admin folder

--- login/app/admin/dashboard/page.js
+++
@@ -1,6 +0,0 @@
-"use client";
-import DashboardContent from "@/app/Components/DashboardContent";
-
-export default function AdminDashboard() {
-  return <DashboardContent title="Admin Dashboard" />;
-}
